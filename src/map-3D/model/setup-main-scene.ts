244e322d5fa38ae328f8251016f5146933ac6aae
deslancer <deslancers@gmail.com>
2022-11-12 00:32:54 +0200

    house selection

--- src/map-3D/model/setup-main-scene.ts
+++ src/map-3D/model/setup-main-scene.ts
@@ -1,5 +1,6 @@
 import {ILoaderService} from "../interfaces/ILoaderService";
 import {useAppStore} from "../../store/store";
+import * as BABYLON from "babylonjs";
 
 export class SetupMainScene {
     private loaderService: ILoaderService;
@@ -13,21 +14,21 @@ export class SetupMainScene {
     setupMeshes() {
         const mainTask = this.loaderService.getMainSceneTask()
         let actionManager = new BABYLON.ActionManager(this.scene);
-        if (mainTask){
+        if (mainTask) {
             mainTask.onSuccess = (task) => {
                 let meshes = task.loadedMeshes;
 
                 meshes.forEach((mesh: BABYLON.Mesh) => {
                     mesh.freezeWorldMatrix();
                     mesh.doNotSyncBoundingInfo = true;
-                    if(mesh.name.includes('Type_')){
+                    if (mesh.name.includes('Type_')) {
                         mesh.instances.map(m => m.actionManager = actionManager)
                     }
 
                 })
 
                 const ground = this.scene.getMeshByName('Road_Plane_1400m_Web');
-                if(ground){
+                if (ground) {
 
                     const ground2KTexture = new BABYLON.Texture('./assets/models/Road_Web_Cut_2K_Color.jpg', this.scene);
                     BABYLON.Texture.WhenAllReady([ground2KTexture], () => {
@@ -50,36 +51,64 @@ export class SetupMainScene {
                             useAppStore.setState({
                                 loading: false
                             })
-                          /*  const ground8KTexture = new BABYLON.Texture('./assets/models/Road_Cut_Web_8K_Color.jpg', this.scene);
-                            BABYLON.Texture.WhenAllReady([ground8KTexture], () => {
-                                //@ts-ignore
-                                ground.material.baseTexture = ground8KTexture;
-                                ground.material?.freeze();
-                                console.log("8k ground texture loaded successfully")
-                                ground2KTexture.dispose();
-                                ground4KTexture.dispose();
-                            })*/
+                            /*  const ground8KTexture = new BABYLON.Texture('./assets/models/Road_Cut_Web_8K_Color.jpg', this.scene);
+                              BABYLON.Texture.WhenAllReady([ground8KTexture], () => {
+                                  //@ts-ignore
+                                  ground.material.baseTexture = ground8KTexture;
+                                  ground.material?.freeze();
+                                  console.log("8k ground texture loaded successfully")
+                                  ground2KTexture.dispose();
+                                  ground4KTexture.dispose();
+                              })*/
                         })
                     })
 
 
                 }
 
-                actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function(ev){
+                actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, (ev) => {
                     const meshUnder = ev.meshUnderPointer;
                     if (meshUnder) {
-                        meshUnder.scaling.x = 1.1;
-                        meshUnder.scaling.y = 1.1;
-                        meshUnder.scaling.z = 1.1;
+                        let childMeshes = meshUnder.subMeshes;
+                        let min = childMeshes[0].getBoundingInfo().boundingBox.minimumWorld;
+                        let max = childMeshes[0].getBoundingInfo().boundingBox.maximumWorld;
+                        let height, width, depth;
+                        for (let i = 0; i < childMeshes.length; i++) {
+                            let meshMin = childMeshes[i].getBoundingInfo().boundingBox.minimumWorld;
+                            let meshMax = childMeshes[i].getBoundingInfo().boundingBox.maximumWorld;
+                            min = BABYLON.Vector3.Minimize(min, meshMin);
+                            max = BABYLON.Vector3.Maximize(max, meshMax);
+                            height = max.y - min.y;
+                            width = max.x - min.x;
+                            depth = (max.z - min.z) * 0.8;
+                        }
+                        if (height) {
+                            const shell_box = BABYLON.MeshBuilder.CreateBox("shell_box" + meshUnder.id, {
+                                height: height,
+                                width: width,
+                                depth: depth
+                            });
+                            shell_box.isPickable = false;
+                            shell_box.position.x = meshUnder.position.x;
+                            shell_box.position.y = height / 2;
+                            shell_box.position.z = meshUnder.position.z;
+                            const emissive_material = new BABYLON.StandardMaterial("shellMaterial" + meshUnder.id, this.scene);
+                            shell_box.material = emissive_material;
+                            emissive_material.alpha = 0.5;
+                            emissive_material.emissiveColor = BABYLON.Color3.FromHexString('#11B39B');
+                            emissive_material.diffuseColor =  BABYLON.Color3.FromHexString('#11B39B');
+
+                        }
                     }
                 }));
                 //if hover is over remove highlight of the mesh
-                actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function(ev){
+                actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger,  (ev) => {
                     const meshUnder = ev.meshUnderPointer;
                     if (meshUnder) {
-                        meshUnder.scaling.x = 1;
-                        meshUnder.scaling.y = 1;
-                        meshUnder.scaling.z = 1;
+                        const shell = this.scene.getMeshByName("shell_box" + meshUnder.id);
+                        const material = this.scene.getMaterialByName("shellMaterial" + meshUnder.id);
+                        shell?.dispose();
+                        material?.dispose();
                     }
                 }));
             }
