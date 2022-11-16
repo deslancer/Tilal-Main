d45cd57f99a8b6ad8bf0a0e8bb16c37294554cf8
deslancer <deslancers@gmail.com>
2022-11-16 23:02:42 +0200

    houses filter

--- src/map-3D/findHouseByProp.ts
+++ src/map-3D/findHouseByProp.ts
@@ -5,12 +5,12 @@ export const findHouseByProp = (prop: string , value: string, state: boolean)=>{
     const housesData = useAppStore.getState().housesData
     const scene = useAppStore.getState().scene
    for(let i = 0; i < housesData.length; i++) {
-       // @ts-ignore
+       // @ts-ignore TODO correct after successful tests
        if(housesData[i][prop].includes(value)){
            if (housesData[i]){
-               // @ts-ignore
+               // @ts-ignore TODO correct after successful tests
                const houseType = housesData[i]['Model'].substring(0,1);
-               // @ts-ignore
+               // @ts-ignore TODO correct after successful tests
                const houseNumber = housesData[i]['LandNumber'];
                const houseMesh = scene.getMeshByName(`Type_${houseType}_${houseNumber}`)
                if (state){
